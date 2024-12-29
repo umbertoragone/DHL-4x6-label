4f07003d8a889a830622649cfa3a91fcf00c400a
Umberto Ragone <umberto@ragone.dev>
2024-12-29 15:59:05 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="w-full text-center text-sm text-neutral-500 p-2">
       <p>
         <a
-          href="https://github.com/umbertoragone/DHL-4x6-label"
+          href="https://github.com/umbertoragone/DHL-4x6-label-cropper"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:underline"
